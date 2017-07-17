e92e303e0c24cebce5766b5d03b31a7f44d0d350
Anne Vetto <vettoap@gmail.com>
2017-07-16 18:44:17 -0700

    basic functionality to merge face components into one face image

--- public/script.js
+++ public/script.js
@@ -15,6 +15,35 @@ angular.module('glitchygifsApp', [])
     vm.character.nose = vm.character.noseList[Math.floor(Math.random()*vm.character.noseList.length)];
     vm.character.mouth = vm.character.mouthList[Math.floor(Math.random()*vm.character.mouthList.length)];
 
+    vm.combineCharacter = function() {
+      var combinedCharacter =document.getElementById("makeCharacterContainerCombined");
+      var ctx= combinedCharacter.getContext("2d");
+      var imageObj1 = new Image();
+      var imageObj2 = new Image();
+      var imageObj3 = new Image();
+      var imageObj4 = new Image();
+      imageObj1.src = vm.character.brows;
+      imageObj1.onload = function() {
+         ctx.drawImage(imageObj1, 15, 0, 50, 25);
+         imageObj2.src = vm.character.eyes;
+         imageObj2.onload = function() {
+            ctx.drawImage(imageObj2, 15, 60, 50, 25);
+          }
+            imageObj3.src = vm.character.nose;
+            imageObj3.onload = function() {
+              ctx.drawImage(imageObj3, 15, 90, 50, 25);
+            }
+              imageObj4.src = vm.character.mouth;
+              imageObj4.onload = function() {
+                ctx.drawImage(imageObj4, 15, 130, 50, 25);
+                var img = combinedCharacter.toDataURL("image/png");
+                document.write('<img src="' + img + '" width="250" height="250"/>');
+         }
+      };
+    };
+
+
+
     // vm.character.brows = '../static/brows1.png';
     // vm.character.eyes = '../static/eyes1.png';
     // vm.character.nose = '../static/nose1.png';
@@ -23,7 +52,8 @@ angular.module('glitchygifsApp', [])
     vm.randomizeFace = function() {
       console.log('im gonna randomize things');
 
-      vm.character.brows = '../static/brows2.png';
+      // vm.character.brows = '../static/brows2.png';
+      vm.combineCharacter();
     }
 
     // var todoList = this;
